c381b4f59a4826ab806b86d82c2d10c94c54953c
정현태 <jht3021@gmail.com>
2026-06-30 17:45:25 +0900

    feat : Board 임시 정의

--- src/main/java/com/raon/tikitaka/domain/board/Board.java
+++ src/main/java/com/raon/tikitaka/domain/board/Board.java
@@ -1,4 +1,21 @@
 package com.raon.tikitaka.domain.board;
 
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.ColumnDefault;
+
+@Entity
+@Table(name = "board")
 public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long board_id;
+
+    //@JoinColumn(name = "match_id", nullable = false)
+    //private Match match;   match 불러와야하는데 아직 안만들어서 보류
+
+    @ColumnDefault("true")
+    @Column(nullable = false)
+    private boolean is_active;
 }
